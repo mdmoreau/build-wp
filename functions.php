9d1f13ea98d44a4f2c8e82cedce66fdc169c6d85
mdmoreau <mdmoreau@gmail.com>
2018-12-28 15:57:30 -0500

    update main css and js filenames

--- functions.php
+++ functions.php
@@ -4,19 +4,19 @@
 function theme_settings() {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
-  add_editor_style([get_theme_file_uri('/dist/css/main.min.css')]);
+  add_editor_style([get_theme_file_uri('/dist/css/main.css')]);
 }
 add_action('after_setup_theme', 'theme_settings');
 
 // theme css
 function theme_css() {
-  wp_enqueue_style('theme-main', get_theme_file_uri('/dist/css/main.min.css'), [], null, 'screen');
+  wp_enqueue_style('theme-main', get_theme_file_uri('/dist/css/main.css'), [], null, 'screen');
 }
 add_action('wp_enqueue_scripts', 'theme_css');
 
 // theme js
 function theme_js() {
-  wp_enqueue_script('theme-main', get_theme_file_uri('/dist/js/main.min.js'), [], null, true);
+  wp_enqueue_script('theme-main', get_theme_file_uri('/dist/js/main.js'), [], null, true);
 }
 add_action('wp_enqueue_scripts', 'theme_js');
 
