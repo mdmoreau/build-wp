08253f1382ea45b38a8f77f43cda5ad7fc3d2692
mdmoreau <mdmoreau@gmail.com>
2019-06-07 14:37:44 -0400

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const postcssColorModFunction = require('postcss-color-mod-function');
 const postcssPresetEnv = require('postcss-preset-env');
 const postcssInlineSvg = require('postcss-inline-svg');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
