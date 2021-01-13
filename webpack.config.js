1e06d2b007e947ce4378d5bc9e481ef3c03b4b33
mdmoreau <mdmoreau@gmail.com>
2021-01-13 06:20:46 -0500

    temp nesting switch

--- webpack.config.js
+++ webpack.config.js
@@ -106,11 +106,12 @@ const config = {
                   'postcss-import',
                   'postcss-mixins',
                   'postcss-root-var',
+                  'postcss-nested',
                   ['postcss-preset-env', {
                     features: {
                       'custom-media-queries': true,
                       'custom-selectors': true,
-                      'nesting-rules': true,
+                      // 'nesting-rules': true,
                     },
                   }],
                   ['postcss-inline-svg', {
