78720b80501f76d297cbaae7f7393683e06d249a
mdmoreau <mdmoreau@gmail.com>
2021-08-30 01:47:22 -0400

    write img to disk during dev

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ const config = {
     open: true,
     host,
     watchFiles: ['**/*.php'],
+    writeToDisk: (filePath) => /img\//.test(filePath),
     proxy: {
       '/': {
         target: `http://${host}`,
