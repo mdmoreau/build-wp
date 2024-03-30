6709e5a99c8fc36d197b1b8ec3a95c2636b91d16
mdmoreau <mdmoreau@gmail.com>
2024-03-29 20:06:58 -0400

    various updates

--- functions.php
+++ functions.php
@@ -3,15 +3,17 @@
 // theme settings
 function theme_settings() {
   add_theme_support('title-tag');
+  add_theme_support('editor-styles');
+  add_editor_style(['/public/dist/style.css']);
 }
 add_action('after_setup_theme', 'theme_settings');
 
 // theme assets
 function theme_assets() {
   if (is_vite()) {
-    wp_enqueue_script('vite-client-dev', 'http://localhost:5173/@vite/client', [], null);
-    wp_enqueue_style('theme-style-dev', 'http://localhost:5173/src/styles/style.css', [], null);
-    wp_enqueue_script('theme-script-dev', 'http://localhost:5173/src/scripts/script.ts', [], null, true);
+    wp_enqueue_script('vite-client', ':5173/@vite/client', [], null);
+    wp_enqueue_style('vite-theme-style', ':5173/src/styles/style.css', [], null);
+    wp_enqueue_script('vite-theme-script', ':5173/src/scripts/script.ts', [], null, true);
   } else {
     wp_enqueue_style('theme-style', get_theme_file_uri('/public/dist/style.css'), [], null);
     wp_enqueue_script('theme-script', get_theme_file_uri('/public/dist/script.js'), [], null, true);
@@ -21,7 +23,7 @@ add_action('wp_enqueue_scripts', 'theme_assets');
 
 // add script module
 function add_script_module($tag, $handle, $src) {
-  $modules = ['vite-client-dev', 'theme-script-dev'];
+  $modules = ['vite-client', 'vite-theme-script'];
   if (in_array($handle, $modules)) {
     $esc_src = esc_url($src);
     return "<script type='module' src='$esc_src'></script>";
@@ -30,16 +32,9 @@ function add_script_module($tag, $handle, $src) {
 }
 add_filter('script_loader_tag', 'add_script_module', 10, 3);
 
-// remove caption padding
-function remove_caption_padding($width) {
-  return $width - 10;
-}
-add_filter('img_caption_shortcode_width', 'remove_caption_padding');
-
 // is vite
 function is_vite() {
-  $host = $_SERVER['HTTP_HOST'];
-  return str_starts_with($host, 'vite') && str_ends_with($host, 'localhost');
+  return wp_get_environment_type() === 'local' && !is_wp_error(wp_remote_get('http://host.docker.internal:5173'));
 }
 
 // inline svg
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
-<h1>index</h1>
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
